5c0f44458f400f544d9aee9b581db5e6810e6a8f
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-02-21 14:48:02 +0100

    Our first real test

--- code/test/pytest/test_using_pytest.py
+++ code/test/pytest/test_using_pytest.py
@@ -1,8 +1,29 @@
 from pytest import approx
 
-
 def test_dummy_pass():
     assert True
 
 def test_dummy_fail():
     assert False
+    
+def test_logtransform():
+    
+    from earlywarningsignals import logtransform
+    
+    # Test 1. Trying a known value
+    
+    # Input and expected output
+    x_in = 0.0
+    y_expected = 0.0
+    
+    # Check it
+    assert (logtransform(x_in) == y_expected)
+    
+    # Test 2. Trying a known value
+    
+    # Input and expected output
+    x_in = 1.0    
+    y_expected = 0.69314718055994529
+    
+    # Check it
+    assert (logtransform(x_in) == y_expected)
