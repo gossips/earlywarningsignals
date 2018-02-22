29936b971ea77e99d040c897fc8d59dbe51b7ab5
WUR\weina005 <els.weinans@wur.nl>
2018-02-22 14:27:15 +0100

    added test for EWS()

--- code/test/pytest/test_using_pytest.py
+++ code/test/pytest/test_using_pytest.py
@@ -28,3 +28,16 @@ def test_logtransform():
     
     # Check it
     assert (logtransform(x_in) == y_expected)
+    
+def test_EWS():
+    from earlywarningsignals import EWS
+    
+    # Test 1. Trying a known value
+    input_ts=np.array([[1,1,6],[2,3,5],[3,4,4],[4,6,1]])
+    output_autocorrelation=[0.92857142857142838, 0.96076892283052284]
+    output_variance=[3.25, 3.5]
+    output_skewness=[0.0, -0.6872431934890912]
+    result=EWS(input_ts,autocorrelation=True,variance=True,skewness=True)
+    assert(result['autocorrelation'] == output_autocorrelation)
+    assert(result['variance'] == output_variance)
+    assert(result['skewness'] == output_skewness)
