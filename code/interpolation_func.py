d0cd1d1ddf2e33f0488c8b8adb2b6d1c980a0c3f
M Usman Mirza <muh.usmanmirza@gmail.com>
2018-02-22 12:05:59 +0100

    Edited documentation

--- code/interpolation_func.py
+++ code/interpolation_func.py
@@ -19,12 +19,12 @@ def interpolate(x, y, new_x = None, dim = 1, method = 'linear', spline = False, 
     dim: Specifies dimension of data. Currently only for 1 or 2 dimensions. Default is 1
     method: Specifies interpolation method used. One of
     	‘nearest’: return the value at the data point closest to the point of interpolation.
-        near’: interpolates linearly between data points on new data points
-        bic’: Interpolated values determined from a cubic spline
-        ault is ‘linear’
-        spline: Spline interpolation. Can be True or False. If True then the function ignores the method call. Default is False. 
-        k: Degree of the smoothing spline. Must be <= 5. Default is k=3, a cubic spline. 
-        der: The order of derivative of the spline to compute (must be less than or equal to k)
+        'linear’: interpolates linearly between data points on new data points
+        'cubic’: Interpolated values determined from a cubic spline
+        Default is ‘linear’
+    spline: Spline interpolation. Can be True or False. If True then the function ignores the method call. Default is False. 
+    k: Degree of the smoothing spline. Must be <= 5. Default is k=3, a cubic spline. 
+    der: The order of derivative of the spline to compute (must be less than or equal to k)
     Created by M Usman Mirza
     """
     if dim == 1 & spline == False:
