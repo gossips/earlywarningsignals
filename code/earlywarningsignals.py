f9b99afc0bd7815e79843e5effea037d5fd914fe
WUR\bolt008 <bregjevanderbolt@gmail.com>
2018-03-29 14:16:15 +0200

    minor changes and part of unit test

--- code/earlywarningsignals.py
+++ code/earlywarningsignals.py
@@ -9,9 +9,8 @@ import pandas as pd
 
 def checkSpacing(iterator):
     iterator=np.asarray(iterator)
-    iterator=iterator.transpose()
     spaced  = iterator[1:]-iterator[0:-1]
-    return len(set(spaced)) <= 1 #set builds an unordered collection of unique elements.
+    return len(set(spaced[:,0])) <= 1 #set builds an unordered collection of unique elements.
 
 def check_time_series(data, timeindex=None):
     ## Dummy function.
@@ -23,17 +22,19 @@ def check_time_series(data, timeindex=None):
     """ 
     if isinstance(data, np.ndarray):
         timeseries = pd.DataFrame(data=data)
+    elif isinstance(data, pd.DataFrame):
+        timeseries= data
     if timeindex is None:
         timeindex = pd.DataFrame(data=np.arange(timeseries.shape[0])) #np.linspace(0,timeseries.shape[0]-1, timeseries.shape[0])
     elif isinstance(timeindex,np.ndarray):
         timeindex = pd.DataFrame(data=timeindex)
     evenly = checkSpacing(timeindex)
     if evenly == False:
-        print("time index is not evenly spaced.")
+        raise ValueError("time index is not evenly spaced.")
     if timeseries.shape[0] == timeindex.shape[0]:
         print("right format for analysis")
     else:
-        print("timeindex and data do not have the same length")
+        raise ValueError("timeindex and data do not have the same length")
     return timeseries, timeindex
 
 def logtransform(df):
