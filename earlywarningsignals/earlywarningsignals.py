243fd691844f727900577b457653e73df22b3c40
Bregje14 <bregjevanderbolt@gmail.com>
2019-09-10 13:08:28 +0200

    Removed individual contributers names + redundant loess-function

--- earlywarningsignals/earlywarningsignals.py
+++ earlywarningsignals/earlywarningsignals.py
@@ -54,17 +54,15 @@ def logtransform(df):
     :param df: dataframe with original timeseries
     :return: new dataframe with logtransformed timeseries
 
-
-    Created by Ingrid van de Leemput
     """
     df=df+1
     df_log=df.apply(np.log)
     return df_log
 
 def detrend(ts, detrending='gaussian', bandwidth=None, span=None, degree=None):
-    
+
     """Detrend time series.
-    
+
     :param ts: data frame with time indices as index and time series values in the columns.
     :param detrending: either
         'gaussian' = Gaussian detrending
@@ -74,115 +72,113 @@ def detrend(ts, detrending='gaussian', bandwidth=None, span=None, degree=None):
     :param span: window size in case of loess, in percentage of time series length. If None, chooses default span (25%).
     :param degree: degree of polynomial in case of loess. If None, chooses default degree of 2.
     :return: trend and residuals. In case of first_diff, returns residuals and difference between consecutive time values.
-    
-    Created by Arie Staal
+
     """
-    
+
     ts_trend = pd.DataFrame().reindex_like(ts)
     ts_residual = pd.DataFrame().reindex_like(ts)
-    
+
     for column in ts:
-    
+
         if detrending == 'gaussian':
-        
+
             if bandwidth == None:
                 # Silverman's rule of thumb
                 bw = round(ts.shape[0] * 0.9 * min(ts[column].std(), (ts[column].quantile(0.75) - ts[column].quantile(0.25)) / 1.34) * ts.shape[0]**(-0.2))
             else:
                 bw = round(ts.shape[0] * bandwidth/100)
-            
+
             trend = gaussian_filter1d(ts[column], bw, axis=0) # smY in R code
             resid = ts[column] - trend                        # nsmY in R code
-        
-        elif detrending == 'linear': 
-        
+
+        elif detrending == 'linear':
+
             x = np.linspace(0, ts.shape[0], ts.shape[0])
             slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x,ts[column])
             trend = intercept + slope * x
             resid = ts[column] - trend
-        
+
         elif detrending == 'loess':
-        
+
             if span == None:
                 span_i = 25/100
             else:
                 span_i = span/100
-            
+
             if degree == None:
                 degree = 2
             else:
                 degree = degree
-            
+
             trend = loess(ts[column], degree=degree, span=span_i)
             resid = ts[column] - trend
-        
+
         # first difference is left out compared to the original R code
-        
+
         ts_residual[column] = resid
         ts_trend[column] = trend
-    
+
     return ts_trend, ts_residual
 
 
 def loess(y, degree=None, span=None):
-    
+
     """Local polynomial regression.
-    
-    Uses weighting of data points after R function loess. 
-    
+
+    Uses weighting of data points after R function loess.
+
     :param y: time series.
     :param degree: degree of polynomial.
     :param span: window size in fraction of time series length.
     :return: trend.
-    
-    Created by Arie Staal
+
     """
-    
+
     x = y.index.values
-    
+
     no_points = int(np.round(span*len(y)))
     half_no_points = int(np.round(0.5*span*len(y)))
-    
+
     maxdist = 0.5*span*len(y)
-    
+
     p = np.empty(np.shape(y))
-    
+
     for i in range(0, len(y)):
 
-        
+
         if i < half_no_points:
             x_span = x[0:no_points]
             y_span = y[0:no_points]
-        
+
         if (i >= half_no_points) & (i <= (len(y) - half_no_points)):
             x_span = x[i - half_no_points : i + half_no_points]
             y_span = y[i - half_no_points : i + half_no_points]
-            
+
         if i > (len(y) - half_no_points):
             x_span = x[len(y)-no_points+1:]
             y_span = y[len(y)-no_points+1:]
-        
+
         w_i = np.empty(np.shape(y_span))
-        
+
         cnt = 0
         for x_i in x_span:
             dist = np.absolute(x[i] - x_i) / (np.max(x) - np.min(x))
             w_i[cnt] = (1 - (dist/maxdist)**3)**3
             cnt = cnt + 1
-        
+
         if len(x_span) <= degree:
             raise TypeError("Window length should be higher than the degree of the fitted polynomial.")
-        
+
         fit = np.poly1d(np.polyfit(x_span, y_span, deg=degree, w=w_i))
         p[i] = fit(i)
-        
+
     return p
 
 
 
 def EWS(ts,window_size=None,autocorrelation=False,variance=False,skewness=False,
         kurtosis_ews=False, CV=False,plots=True):
-    
+
     """Function that calculates early warning signals
 
     :param timeseries: Original timeseries (column for every variable)
@@ -195,25 +191,25 @@ def EWS(ts,window_size=None,autocorrelation=False,variance=False,skewness=False,
     :param CV: Set to True if coefficient of variation is required
     :return: pandas dataframe with variable, and the chosen early warning indicators for every window
     """
-    
+
     nr_vars=ts.shape[1]
-    
+
     if window_size == None:
         window_size = len(ts.iloc[:,0])
-        
+
     # The functions that calculate the EWS
-    a=lambda x: np.corrcoef(x[1:],x[:-1])[1,0]  
-    b=lambda x: np.var(x)  
+    a=lambda x: np.corrcoef(x[1:],x[:-1])[1,0]
+    b=lambda x: np.var(x)
     c=lambda x: scipy.stats.skew(x[:])
     d=lambda x: scipy.stats.kurtosis(x[:])
     e=lambda x: np.std(x[:])/np.mean(x[:])
-    
+
     functions=[a,b,c,d,e]
     indicators=[autocorrelation,variance,skewness,kurtosis_ews,CV]
     strindicators=['autocorrelation','variance','skewness','kurtosis_ews','CV']
     idx=np.where(indicators)[0]
     strindicators=[strindicators[i] for i in idx] #Only calculate the selectd indicators
-    
+
     for n in range(nr_vars):
         df1=ts.iloc[:,n]
         res=df1.rolling(window_size).agg([functions[i] for i in idx])
@@ -222,7 +218,7 @@ def EWS(ts,window_size=None,autocorrelation=False,variance=False,skewness=False,
         if n == 0:
             result = res
         else:
-            result=pd.concat([result,res],axis=0)            
+            result=pd.concat([result,res],axis=0)
     if plots == True:
         for i in strindicators:
             result.groupby('variable')[i].plot(legend=True,title=i)
@@ -241,7 +237,7 @@ def kendalltrend(ts):
     tau, p_value = scipy.stats.kendalltau(ti,ts)
     return [tau, p_value]
 
-        
+
 
 #Interpolation function
 def interp(df, new_x, method = 'linear', spline = False, k = 3, s = 0, der = 0):
@@ -259,7 +255,7 @@ def interp(df, new_x, method = 'linear', spline = False, k = 3, s = 0, der = 0):
         :param s: Smoothing value. Default is 0. A rule of thumb can be s = m - sqrt(m) where m is the number of data-points being fit.
         :param der: The order of derivative of the spline to compute (must be less than or equal to k)
         :rtype: Dataframe of interpolated values
-        Created by M Usman Mirza
+
     """
     df_x = df.index.tolist()
     df_new = pd.DataFrame(columns = df.columns, index = new_x)
@@ -268,72 +264,22 @@ def interp(df, new_x, method = 'linear', spline = False, k = 3, s = 0, der = 0):
             f = interpolate.interp1d(x = df_x, y = df[y].tolist(), kind = method)
             i = f(new_x)
             df_new[y] = i
-            
+
         elif spline == True:
             f = interpolate.splrep(x = x, y = df[y].tolist(), k = k, s = s)
             i = interpolate.splev(x = new_x, tck = f, der = der)
             df_new[y] = i
     return df_new
 
-def loess(x, y, degree, span):
-
-    """Local polynomial regression.
-
-    Uses weighting of data points after R function loess.
-
-    :param x: times series indices.
-    :param y: time series.
-    :param degree: degree of polynomial.
-    :param span: window size in fraction of time series length.
-    :return: trend.
-
-    Created by Arie Staal
-    """
-
-    no_points = int(np.round(span*len(y)))
-    half_no_points = int(np.round(0.5*span*len(y)))
-
-    maxdist = 0.5*span*len(y)
-
-    p = np.empty(np.shape(y))
-
-    for i in range(0,len(y)):
-
-        if i < half_no_points:
-            x_span = x[0:no_points]
-            y_span = y[0:no_points]
-
-        if (i >= half_no_points) & (i <= len(y) - half_no_points):
-            x_span = x[i - half_no_points : i + half_no_points]
-            y_span = y[i - half_no_points : i + half_no_points]
-
-        if i > (len(y) - half_no_points):
-            x_span = x[len(y)-no_points+1:]
-            y_span = y[len(y)-no_points+1:]
-
-        wi = np.empty(np.shape(y_span))
-
-        cnt = 0
-        for x_i in x_span:
-            dist = np.absolute(x[i] - x_i) / (np.max(x) - np.min(x))
-            w_i[cnt] = (1 - (dist/maxdist)**3)**3
-            cnt = cnt + 1
-
-        fit = np.poly1d(np.polyfit(x_span, y_span, deg=degree, w=w_i))
-        p[i] = fit(i)
-
-    return p
-
 
 def krig(df, buf, new_index, N):
     """Interpolates data using Kriging.
-    :param df: Dataframe to interpolate with time as the index.  
+    :param df: Dataframe to interpolate with time as the index.
     :param buf: The bandwidth for the empirical variogram
     :param new_index: The new time index on which to get interpolated points
-    :param N: Number of nearest points to consider for interpolation. 
-    :return point estimates, lags, emprircal variogram, model variogram 
-    
-    Created by M Usman Mirza
+    :param N: Number of nearest points to consider for interpolation.
+    :return point estimates, lags, emprircal variogram, model variogram
+
     """
     #empirical variogram
     def vario(df, buf):
@@ -345,11 +291,11 @@ def krig(df, buf, new_index, N):
         sq_dist = spatial.distance.squareform(dist)
         sv_lag = []
         sv = []
-        for k in lags:   
+        for k in lags:
             for i in range(df_l):
                 for j in range(i + 1, df_l):
                     if (sq_dist[i, j] >= k - buf) and (sq_dist[i, j] <= k + buf):
-                        sv_lag.append((df.iloc[i] - df.iloc[j])**2)    
+                        sv_lag.append((df.iloc[i] - df.iloc[j])**2)
             sv.append(sum(sv_lag)/(2*len(sv_lag)))
         return array(sv), lags
 
@@ -361,16 +307,16 @@ def krig(df, buf, new_index, N):
         return sill - sv[lag_i]
 
     #spherical variogram model
-    def sph_m(lags, a, c, nugget): 
+    def sph_m(lags, a, c, nugget):
         sph = []
         for i in range(lags.size):
-            if lags[i] <= a:  
+            if lags[i] <= a:
                 sph.append(c*( 1.5*lags[i]/a - 0.5*(lags[i]/a)**3.0) + nugget)
             if lags[i] > a:
                 sph.append(c + nugget)
-        return sph 
-    
-    
+        return sph
+
+
     def vario_fit(df, buf):
         sv, lags = vario(df, buf) #empirical variogram
         c = c_f(df, lags[0], 0, sv) #sill - nugget
@@ -381,7 +327,7 @@ def krig(df, buf, new_index, N):
         sv_model = sph_m(lags, sph_par[0], sph_par[1], sph_par[2])
         return lags, sv, sv_model, sph_par
 
-    
+
     lags, sv, sv_model, sph_par = vario_fit(df, buf)
     mu = array(mean(df))
     coord_df = array([repeat(0, array(df.index).size), array(df.index)]).T
@@ -390,7 +336,7 @@ def krig(df, buf, new_index, N):
     dist_mat = c_[df.index, df, dist_mat]
     int_e = []
     for i in range(len(new_index)):
-        dist_mat_1 = dist_mat[dist_mat[:,i+2].argsort()] 
+        dist_mat_1 = dist_mat[dist_mat[:,i+2].argsort()]
         dist_mat_1 = dist_mat_1[:N,:]
         k = sph_m(dist_mat_1[:,i+2], sph_par[0], sph_par[1], sph_par[2])
         k = matrix(k).T
@@ -402,4 +348,3 @@ def krig(df, buf, new_index, N):
         int_e.append(resid*weights + mu)
     int_e = ravel(int_e)
     return int_e, lags, sv, sv_model
-
